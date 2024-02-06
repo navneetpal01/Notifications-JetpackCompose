dd80e19d3ad752d3e7d913e61e183e2415ec13d0
navneetpal01 <navneetpalgithub@gmail.com>
2024-02-06 12:10:36 +0530

    New Barnch

---
+++ app/src/main/java/com/example/notificationjetpackcompose/receiver/MyReceiver.kt
@@ -0,0 +1,21 @@
+package com.example.notificationjetpackcompose.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+//BroadCast Receivers are mostly used to receive some messages from the android System itself or from other Applications as well
+class MyReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val message = intent?.getStringExtra("Message")
+        if (message!=null){
+            Toast.makeText(
+                context,
+                message,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+}
